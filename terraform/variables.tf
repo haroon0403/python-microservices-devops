ba32be19e7e6803c2f56e27ec0a6f7e8f42ecbd3
mohammad haroon khan <32263261+haroon0403@users.noreply.github.com>
2025-09-03 15:50:49 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,7 +2,7 @@ variable "aws_region" {
   default = "us-east-1"
 }
 variable "instance_type" {
-  default = "t2.micro"
+  default = "t3.micro"
 }
 variable "key_name" {
   description = "EC2 key pair"
